7db06696e394519e3582d815f9116522ccfa480e
Benedikt Lotter <blotter123@googlemail.com>
2014-07-25 01:15:10 -0400

    score calculation logic

---
+++ Play4Pay/PGScoreCalculator.h
@@ -0,0 +1,13 @@
+//
+//  PGScoreCalculator.h
+//  Play4Pay
+//
+//  Created by Benedikt Lotter on 7/24/14.
+//  Copyright (c) 2014 Play4Pay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PGScoreCalculator : NSObject
+
+@end
---
+++ Play4Pay/PGScoreCalculator.m
@@ -0,0 +1,13 @@
+//
+//  PGScoreCalculator.m
+//  Play4Pay
+//
+//  Created by Benedikt Lotter on 7/24/14.
+//  Copyright (c) 2014 Play4Pay. All rights reserved.
+//
+
+#import "PGScoreCalculator.h"
+
+@implementation PGScoreCalculator
+
+@end
