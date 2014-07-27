f1a91e7ddebaebba3d2862d8947251edef0f6f68
Benedikt Lotter <blotter123@googlemail.com>
2014-07-27 16:37:44 -0400

    saving user fb data in plist, data service singleton, refactor post/get score

---
+++ Play4Pay/PGDataService.h
@@ -0,0 +1,13 @@
+//
+//  PGDataService.h
+//  Play4Pay
+//
+//  Created by Benedikt Lotter on 7/27/14.
+//  Copyright (c) 2014 Play4Pay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PGDataService : NSObject
+
+@end
---
+++ Play4Pay/PGDataService.m
@@ -0,0 +1,13 @@
+//
+//  PGDataService.m
+//  Play4Pay
+//
+//  Created by Benedikt Lotter on 7/27/14.
+//  Copyright (c) 2014 Play4Pay. All rights reserved.
+//
+
+#import "PGDataService.h"
+
+@implementation PGDataService
+
+@end
